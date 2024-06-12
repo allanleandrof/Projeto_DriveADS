3d4838b7eaa8b9151a959ac2575cc3832f14fd28
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-06-12 07:58:46 -0300

    feat: adiciona tarefa agendada que ira executar a cada 10 segundos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import admin from 'firebase-admin';
+import schedule from 'node-schedule';
 
 enum MetricType {
   OPEN_QR = 'NumOpenQR',
@@ -6,48 +7,45 @@ enum MetricType {
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert('serviceMetridoorsKey.json.json'),
+  credential: admin.credential.cert('serviceMetridoorsKey.json'),
 });
 
-admin.firestore().collection('metrics').get()
-  .then(response => {
-    const updatePromises = response.docs.map(doc => {
-      const data = doc.data();
-      let newValue;
-
-      if (data.metricName === MetricType.OPEN_QR) {
-        const previousValue = data.value || 0;
-        const randomFactor = 0.9 + Math.random() * 0.2;
-
-        newValue = Math.ceil(previousValue * randomFactor);
-      } else if (data.metricName === MetricType.NUM_PASSED) {
-        const previousValue = data.value || 0;
-        const randomIncrement = Math.floor(Math.random() * 5) + 1; // Incremento aleatório entre 1 e 5
-
-        newValue = previousValue + randomIncrement;
-      } else {
-        // Se a métrica não for "NumOpenQR" nem "NumPassed", mantém o valor atual
-        return Promise.resolve();
-      }
-
-      return doc.ref.update({ value: newValue });
-    });
-
-    // Executa todas as atualizações em paralelo
-    return Promise.all(updatePromises);
-  })
-  .then(() => {
-    console.log('Valores das métricas "NumOpenQR" e "NumPassed" atualizados com sucesso');
-  })
-  .catch(error => console.error('Erro ao atualizar valores das métricas:', error));
-
-admin.firestore().collection('metrics').get()
+/**
+ * Tarefa agendada que executa a cada 10 segundos
+ * 
+ * A cada 10 segundos as métricas OPEN_QR e NUM_PASSED são atualizadas aleatoriamente,
+ * assim simulando a captura do sistema de monitoramento do Outdoor.
+ */
+schedule.scheduleJob('*/10 * * * * *', function() {
+  admin.firestore().collection('metrics').get()
     .then(response => {
-        const datas = response.docs.map(doc => ({
-            ...doc.data(),
-            uid: doc.id,
-        }));
+      const updatePromises = response.docs.map(doc => {
+        const data = doc.data();
+        let newValue;
 
-        console.log(datas);
+        if (data.metricName === MetricType.OPEN_QR) {
+          const previousValue = data.value || 0;
+          const randomFactor = 0.9 + Math.random() * 0.2;
+
+          newValue = Math.ceil(previousValue * randomFactor);
+        } else if (data.metricName === MetricType.NUM_PASSED) {
+          const previousValue = data.value || 0;
+          const randomIncrement = Math.floor(Math.random() * 5) + 1; // Incremento aleatório entre 1 e 5
+
+          newValue = previousValue + randomIncrement;
+        } else {
+          // Se a métrica não for "NumOpenQR" nem "NumPassed", mantém o valor atual
+          return Promise.resolve();
+        }
+
+        return doc.ref.update({ value: newValue });
+      });
+
+      // Executa todas as atualizações em paralelo
+      return Promise.all(updatePromises);
     })
-    .catch(error => console.error(error));
+    .then(() => {
+      console.log('Valores das métricas "NumOpenQR" e "NumPassed" atualizados com sucesso');
+    })
+    .catch(error => console.error('Erro ao atualizar valores das métricas:', error));
+});
