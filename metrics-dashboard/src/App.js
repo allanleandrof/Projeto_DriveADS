8ca9385bd17c35832a499dd1f89249a4497a556a
Allan Leandro <alanleandrof@gmail.com>
2024-06-13 15:01:51 -0300

    feat: adiciona grafico das metricas e estilização

--- metrics-dashboard/src/App.js
+++ metrics-dashboard/src/App.js
@@ -1,16 +1,14 @@
-// src/App.js
 import React from "react";
 import Dashboard from "./components/Dashboard";
-//import './App.css';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Meu Aplicativo</h1>
+    <div className="App">
+      <h1>Metridoor #1</h1>
       <Dashboard />
     </div>
   );
 };
 
 export default App;
-
--- metrics-dashboard/src/components/Dashboard.js
+++ metrics-dashboard/src/components/Dashboard.js
@@ -1,18 +1,17 @@
-// src/components/Dashboard.js
-
 import React, { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+import Chart from "chart.js";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
+  const [dataList, setDataList] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, "metrics"));
-        const dataList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setData(dataList);
+        const newDataList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setDataList(newDataList);
       } catch (error) {
         console.error("Erro ao buscar dados do Firestore:", error);
       }
@@ -21,18 +20,44 @@ const Dashboard = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (dataList) {
+      const ctx = document.getElementById("myChart").getContext("2d");
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: dataList.map(item => item.metricName), 
+          datasets: [{
+            label: "Valor",
+            data: dataList.map(item => item.value),
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+    }
+  }, [dataList]);
+
   return (
-    <div>
-      <h1>Dados do Firestore</h1>
-      <ul>
-        {data.length > 0 ? (
-          data.map(item => (
-            <li key={item.id}>{JSON.stringify(item)}</li>
-          ))
+    <div className="dashboard-container">
+      <h1 className="dashboard-title">Dashboard</h1>
+      <div className="chart-container">
+        {dataList ? (
+          <canvas id="myChart" className="chart"></canvas>
         ) : (
           <p>Carregando dados...</p>
         )}
-      </ul>
+      </div>
     </div>
   );
 };
